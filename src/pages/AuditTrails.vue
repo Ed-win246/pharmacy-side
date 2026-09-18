ad5946155cf8692fa50afb765445e2f1f07fa71a
Ed-win246 <edwinatuhaire96@gmai.com>
2026-09-18 08:59:04 +0300

    new vue pages and working on one by one

---
+++ src/pages/AuditTrails.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+<template>
+    <div class="min-h-screen w-full">
+        <div class="max-w-7xl h-full max-auto overflow-y-auto pb-5 p-2 flex-col">
+            <div class="flex items-center justify-between border-b border-gray-400 px-2 py-1">
+                <div>
+                    <h2 class="font-medium text-sm uppercase flex items-center gap-2"> Audit Trails</h2>
+                </div>
+                <div>
+                    <h2 class="font-medium text-sm">
+                        <router-link to="/dashboard" class="font-medium">Dashboard</router-link>
+                        <span class="text-gray-400">/ Audit Trails</span>
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
