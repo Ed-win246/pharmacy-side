bf6935dd56392bb96cde22ee112db61f3c85c050
Ed-win246 <edwinatuhaire96@gmai.com>
2026-09-18 14:04:51 +0300

    new pages to work with

---
+++ src/pages/CategoryExpense.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+<template>
+    <div class="min-h-screen w-full">
+        <div class="h-full max-w-7xl max-auto overflow-y-auto pb-5 p-2">
+            <div class="flex items-center justify-between border-b border-gray-400 px-2 py-1">
+                <div>
+                    <h2 class="font-medium uppercase flex items-center gap-2">Category Expenses</h2>
+                </div>
+                <div class="font-medium text-sm">
+                    <router-link to="/dashboard" class="cursor-pointer">Dashboard</router-link>
+                    <span class="text-gray-400">/ Expense Category</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
