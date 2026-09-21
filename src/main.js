d300e91265fe16b8ca3f34cfa32f1e01ae152411
Ed-win246 <edwinatuhaire96@gmai.com>
2026-09-21 15:53:14 +0300

    invoking the pinia call

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 app.use(router)
+app.use(createPinia())//pinia 
 app.mount('#app')
